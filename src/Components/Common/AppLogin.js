91fa43fa878047ec7fd1e210c4761dc94d0edaec
Talha Naseer <talha.naseer@ku,edu>
2024-04-14 02:43:16 -0500

    new

--- src/Components/Common/AppLogin.js
+++ src/Components/Common/AppLogin.js
@@ -4,9 +4,9 @@ import Button from 'react-bootstrap/Button';
 function AppLogin() {
   const buttonStyle = {
     borderRadius: '6px', // Adjust button border radius
-    backgroundColor: '#2E2C2C', // Adjust button background color
+    backgroundColor: '#726060', // Adjust button background color
     borderColor: '#EEEEEE', // Adjust button border color
-    color: 'white', // Adjust button text color
+    color: '#EEEEEE', // Adjust button text color
     fontWeight: 'bold', // Adjust button text weight
     padding: '10px 20px', // Adjust button padding
     fontSize: '16px' // Adjust button font size
@@ -15,7 +15,7 @@ function AppLogin() {
   return (
     <div>
       <Button variant="none" style={buttonStyle}>
-        Login or Create an Account to Start Making a Difference
+      Join Us to Make a Difference
       </Button>
     </div>
   );
